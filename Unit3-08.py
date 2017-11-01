af0a4ffabee376bee9f3717f14a1df91ca37fc19
Peter  Zhu <peter.zhu@ocsbstudent.ca>
2017-11-01 13:49:45 -0400

    Init

---
+++ Unit3-08.py
@@ -0,0 +1,16 @@
+# Created by: Peter Zhu
+# Created on: Oct 23th
+# Course: ICS3U
+# unicode program
+
+for number in range(65,91):
+	  unicode = unichr(number)
+	  print(str(number) + ' -> '+ str(unicode))
+	  
+for number in range(97,123):
+	  unicode = unichr(number)
+	  print(str(number) + ' -> '+ str(unicode))
+
+
+
+
